2acbbd3fc3858fec0ce9cb7f7a9154dffc65b144
Damian9151 <146436874+Damian9151@users.noreply.github.com>
2023-11-07 12:10:18 +0100

    Add files via upload

---
+++ mystory2.js
@@ -0,0 +1,102 @@
+let readlineSync = require('readline-sync');
+
+function stukjee1(){
+console.log("")
+console.log("You wake up in bed still sleepy you slowly stand up to turn on the lights *click* you walk back to your bed to grab your phone too see how late it is you realize that the little nap you just took was way longer than expected because its now 21:23 PM you walk out of your room but you realize no ones home theres a note on the fridge its says your dad and I went to go grab dinner we wont be home until tomorrow I left you some money on the table to buy you something form the story and dont stay up too late again love you lots mom- You to the bathroom too wash your face and to get ready to go outside but you realize its now 21:43 PM and the store closes at 22:00 you hastily put on your shoes and before you run out you go back to grab the money you almost forgot on the table you run fast because its almost closing time but on the a crosswalk you see in the distance a girl who looks a little drunk walking on the street but in the distance you see someone speeding you yell at her to get out of the way but she doesnt hear you you run as fast as you can and you push her out of the way but then *bam* you got hit by the car and everything feels light like a dream as you look at the night sky still processing what just happened you slowly close your eyes You feel like you could sleep for ages it seemed like you were in darkness for hours maybe even days as you think this eternal darkness will maybe last forever a bright light shines as you slowly try to open your eyes it feels like youre staring at a bright sun you see a someone a Silhouette slowly coming close as it comes closer it looks like it has wings angelic wings like a angle it look beautiful as it finally stops right infront of you and says *???*: Hello Dear child you seem quite lost. *You*: w-w-Where am I? Who who are you?!? *???*: I am what most people call a Deity and I am decide the judgement of all people Who died *You*: what will happen to me now will I go to heaven or will something else happen to me. *Deity*: I will give you the option to either to go back to your old life or transported to another world in that world it will be very different in that world, but Ill give you the necessary skills to survive in that world. You have two options:")
+console.log("")
+console.log("A:You decide that you will transport to the other world *stukje 5*")
+console.log("B:You decide that you rather would want to be transported to your old life *stukje 2*") 
+
+}
+
+
+ function stukje3(){
+    console.log("")
+    console.log("Texting to him You: yeah sure at what time should I be there Saad: Wel its now 4pm and the party where we are going start at 7pm so around 6 pm we will meet each other and form there we will go to the party You: alright see you soon. You walk from school to your home where you start getting ready for the party When you finally finish getting ready you go to Saad his house and from there you both go to the party but when you get there you both see that no one was there you ask Saad why no one has arrived yet and he checks what happened turns out that the police arrived because a fight broke out and now you both wasted your time you. You have one option:")
+    console.log("")
+    console.log("A: Go home *stukje 4*")
+    console.log("") 
+    }
+
+function stukje4(){
+    console.log("")
+    console.log("When you get home you decide to make some food and do some of your homework after a bit you finish your homework and food and decide to watch a movie after that you decide to go to sleep and than it happened you see yourself pushing away a women away and a car running you over and you then see what really happened and see why that day was so strange you decide you should really life your life to the fullest and decide to give everything your all because life is short.")
+    console.log("")
+    console.log("")
+    console.log("") 
+    }
+ 
+ function stukje5(){
+    console.log("")
+    console.log("As you stand in the lush forest, you cant help but feel a sense of unease. The world around you is both wondrous and unfamiliar. The blue window displaying your stats and skills hovers in the air before you, and you decide to explore it further. Your Stats: Name: [unknown] Age: [17] Level: 1 HP (Health Points): 100/100 MP (Magic Points): 50/50 Strength: 10 Agility: 12 Intelligence: 15 Charisma: 8 Luck: 5 Your Skills: [Basic Magic]: You have the ability to harness and cast elemental magic, including fire, water, earth, and air. [Status Analysis]: You can assess the status of creatures and objects, gaining insight into their health, attributes, and weaknesses. With your newfound skills and abilities, you decide to explore this mysterious world. The forest is teeming with life, and you can hear the chirping of birds and the rustling of leaves. Its a beautiful place, but you cant help but wonder about the hardships the Deity mentioned. What challenges lie ahead, and how will you overcome them? As you walk deeper into the forest, you come across a small stream. The water sparkles in the dappled sunlight, and you realize that youre feeling a bit hungry. Its time to test your skills and see if you can gather food using your newfound abilities. You focus on your [Basic Magic] skill and try to conjure a small fire to cook any potential food you might find. Then, you activate [Status Analysis] to assess the creatures and plants around you for edibility and potential dangers. Youre feeling hungry and decide to gather food in the forest. You have two options:")
+    console.log("")
+    console.log("A: Use your [Basic Magic] skill to conjure a small fire and try to catch some game or fish. *stukje 6*")
+    console.log("B: Activate your [Status Analysis] skill to assess the creatures and plants around you to find edible options. *stukje 7*") 
+    }
+ 
+ function stukje6(){
+    console.log("")
+    console.log("You decide to use your [Basic Magic] skill to conjure a small fire. You concentrate on your newfound powers and create a flickering flame in the palm of your hand. With the fire ready, you begin to scout for potential game or fish. As you explore the area near the stream, you come across a clearing with a small, tranquil pond. You notice movement near the waters edge, and you see a group of fish swimming lazily. This seems like a perfect opportunity to catch something to eat. You extend your hand toward the pond, directing your magical energy to control the water element. With a swift motion, you create a small whirlpool, drawing the fish closer to the surface. You manage to catch a few of them with your magic, and soon, you have a handful of fish wriggling in the air. Now, its time to cook your catch over the fire you created. You place the fish over the flame and watch as they sizzle and cook to perfection. The aroma is tantalizing, and your stomach grumbles in anticipation. After a few minutes, your meal is ready. You savor the delicious, freshly cooked fish, and your hunger is satisfied. You sit by the pond, contemplating your situation. The forest is peaceful, and you wonder what other adventures and challenges await you in this new world. With a full belly, you're ready to explore further. What would you like to do next? You have two options:")
+    console.log("")
+    console.log("A: Try to make contact with any nearby inhabitants, if there are any.(stukje 11) ")
+    console.log("B: Continue exploring the forest. (stukje 7)") 
+    }
+
+function stukje7(){
+    console.log("")
+    console.log("You decide to activate your [Status Analysis] skill to assess the creatures and plants around you for edible options. The blue window displaying your skill appears, and you start scanning your surroundings. As you observe the creatures and plants, you discover the following: Theres a variety of edible berries and nuts growing on nearby bushes and trees. They seem safe to consume. You notice a group of rabbits hopping through the underbrush. They could be potential game for a meal. Some mushrooms are scattered across the forest floor. Youre not sure if theyre edible or poisonous; your analysis skill cant provide a definitive answer. You reach out to gather the berries and nuts, and you carefully approach the rabbits, trying to catch one for your meal. As you inch closer to a rabbit, ready to make your move, a sudden rustling in the bushes behind you catches your attention. A strange creature emerges, looking like a mix of a wolf and a lizard. It has sharp teeth and is clearly not friendly. It growls at you, and you have a split-second decision to make: You have two options:")
+    console.log("")
+    console.log("A: Choose to run and evade the creature, hoping to escape unharmed. (stukje 10)")
+    console.log("B: Try to fight the creature with your [Basic Magic] skill.(stukje 8) ") 
+    }
+
+    function stukje8(){
+        console.log("")
+        console.log("In a moment of panic, you decide to stand your ground and fight the strange creature with your [Basic Magic] skill. You summon a burst of fire, launching it towards the creature in an attempt to defend yourself. The fiery projectile hits the creature, but then after the smoke clear form the magic attack fire you see that the creature took no damage and you just made the creature angry by attacking it it growls at you as it runs towards you at the creature is way to fast so you got bit by the creature you use your skill [Status Analysis]  and see that the creature is level 16 and the stats of the creature are way higher than yours and at that moment the creature gets ready to attack again you have a split-second decision to make: You have one options:")
+        console.log("")
+        console.log("A: RUNNNN!!! (stukje 9)")
+        console.log("") 
+        }
+
+function stukje2t4(){
+    console.log("")
+    console.log("You: I would like to go back to my old life Deity: Very well dear child the transporting will be begin soon You slowly open your eyes you are in a bed again you stand up to turn on the lights and youre in your room again it felt like something happened, but you forgot you look at the time and its 7 AM! Did I really sleep that long I should make something to eat you got the fridge but there is nothing inside huh? I really thought I bought something for the store I guess ill just buy something form the vending machine at school you get ready for school and grab your bag and leave its just a normal school day until you get a text by someone its Saad your friend and he asked if you would like to go to a party today You have two options:")
+    console.log("")
+    console.log("A: I cant go I still have homework to do at home *stukje 4*")
+    console.log("B: Yeah sure at what time should I be there *stukje 3*") 
+
+    let antwoord2 = readlineSync.question("A/B: ");
+    if (antwoord2 === "A") {
+        stukje4()
+    }else if (antwoord2 === "B") {
+        stukje3()
+    }
+let antwoord3 = readlineSync.question("A: ");
+if (antwoord3 === "A") {
+    stukje4()
+}
+}
+
+
+
+
+
+stukjee1()
+let antwoord = readlineSync.question("A/B: ");
+    if (antwoord === "A") {
+        stukje5()
+    }else if (antwoord === "B") {
+        stukje2t4()
+    }
+
+let antwoord1 = readlineSync.question("A/B: ");
+    if (antwoord1 === "A") {
+        stukje6();
+    }else if (antwoord1 === "B") {
+        stukje7();
+    }
+
+    let antwoord4 = readlineSync.question("A: ");
+    if (antwoord4 === "A") {
+        stukje8();
+    }
